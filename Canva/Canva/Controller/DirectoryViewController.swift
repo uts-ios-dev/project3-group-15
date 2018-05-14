19d7f91a6ecef1bb1cf25e93839560b89e45be61
Thien Nguyen <quangthien27@gmail.com>
2018-05-14 14:32:49 +1000

    Init sample views

---
+++ Canva/Canva/Controller/DirectoryViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Canva
+//
+//  Created by Thien Nguyen on 5/14/18.
+//  Copyright © 2018 Thien Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class DirectoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
