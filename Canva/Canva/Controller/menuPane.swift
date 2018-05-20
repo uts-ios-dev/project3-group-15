11afa55afa573e7828dcabc44944c1e5561993c1
MJClarke93 <Mitchell.J.Clarke@student.uts.edu.au>
2018-05-21 05:50:36 +1000

    Started preparing main screen UI

---
+++ Canva/Canva/Controller/menuPane.swift
@@ -0,0 +1,29 @@
+//
+//  menuPane.swift
+//  Canva
+//
+//  Created by Brenda Brown on 21/5/18.
+//  Copyright © 2018 Thien Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class menuPane: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
+
+class menuPaneLeft: menuPane {
+    // To be written
+}
+
+class menuPaneRight: menuPane {
+    // To be written
+}
