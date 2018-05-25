e3ff9940c635dee649c4127f61e061cc3bc5ece8
Thien Nguyen <quangthien27@gmail.com>
2018-05-25 16:22:54 +1000

    tweak

---
+++ Canva/Canva/Model/Sticker.swift
@@ -0,0 +1,16 @@
+//
+//  Sticker.swift
+//  Canva
+//
+//  Created by Thien Nguyen on 5/21/18.
+//  Copyright © 2018 Thien Nguyen. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class Sticker: UIButton {
+    
+    
+    
+}
